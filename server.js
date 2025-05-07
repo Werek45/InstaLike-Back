1e2ff0cb6fa467fd355b42c20e49b5215a44f503
Werek Silva <93212317+Werek45@users.noreply.github.com>
2025-05-07 16:23:16 -0300

    Criação de base de dados, adicionando rotas e preparando o MongoDb para subir o recurso na nuvem

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 import express from "express";
 
-const app = express();
+const posts = [//lista de dados de posts //Array em memoria
+    {id: 1, descricao: "Paisagem de montanhas ao amanhecer",imagem: "https://images.unsplash.com/photo-1506744038136-46273834b3fb",},
+    {id: 2, descricao: "A beleza de um novo dia nos campos verdejantes.", imagem: "https://images.unsplash.com/photo-1518548414898-299cdb635378",},
+    {id: 3, descricao: "A serenidade do lago refletindo o céu azul.", imagem: "https://images.unsplash.com/photo-1501785888023-7017b79c136c",},
+];
+    
+    const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas vindas à imersão");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPostPorId(id) {
+    return posts.findIndex((post)=> {
+        return post.id === Number(id);
+    });
+}// dois pontos : significa que essa informação sera substituida por uma variavel 
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorId(req.params.id)
+    res.status(200).json(posts[index]);
+    
 });
