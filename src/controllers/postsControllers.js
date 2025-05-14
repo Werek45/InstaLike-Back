6675eb50db073b854c5c6e0576b769a6c9a85d8c
Werek Silva <93212317+Werek45@users.noreply.github.com>
2025-05-13 23:00:59 -0300

    adicionando POST para criar novos posts

--- src/controllers/postsControllers.js
+++ src/controllers/postsControllers.js
@@ -1,4 +1,4 @@
-import { getTodosPosts } from "../models/postsModel";
+import { getTodosPosts, criarPost} from "../models/postsModel.js";
 
 export async function listarPosts(req, res) {
     // Chama a função getTodosPosts() para obter todos os posts do banco de dados de forma assíncrona
@@ -6,3 +6,15 @@ export async function listarPosts(req, res) {
     // Envia a resposta HTTP com o código de status 200 (OK) e o array de posts no formato JSON
     res.status(200).json(posts);
     }
+
+export async function postarNovoPost(req, res) {
+        const novoPost = req.body;
+        try {
+            postCriado = await criarPost(novoPost);
+            res.status(200).json(postCriado);
+        } catch (erro) {
+            console.error(`Falha na conexão com o banco! erro `+ erro.message);
+            //Não dar tantos detalhes na parte da requisição para quem esta usando, como tipo de erro.message
+            res.status(500).json({"Erro":"Falha na requisição"});
+        }
+}
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsControllers.js";
+import { listarPosts, postarNovoPost} from "../controllers/postsControllers.js";
 
 const routes = (app) => {
     // Configura o middleware express.json(), que permite que o servidor analise o corpo das requisições HTTP que possuem o cabeçalho Content-Type: application/json
     app.use(express.json());
     // Define uma rota GET no endpoint '/posts' para retornar todos os posts do banco de dados
     app.get("/posts", listarPosts);
+    //Define uma rota POST para Rota para criar um post
+    app.post("/posts", postarNovoPost)
 }
 
 export default routes;
